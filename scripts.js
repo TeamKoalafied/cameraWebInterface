41cd0108cbcc206b701ba7b427a8b9ae9111364e
Emil Smith <smithomaster@gmail.com>
2019-03-07 21:40:00 +1030

    line Draw

--- scripts.js
+++ scripts.js
@@ -1,58 +1,19 @@
+$(document).ready(function(){
 
-function wideWidth() {
-  var x1;
+    var imageDpi = 300;
 
-  x1 = document.getElementById("wideWidth").value;
+    var can = document.getElementById('rotatedCanvas');
+    var ctx = can.getContext('2d');
+    var startX, startY;
 
-  if (isNaN(x1)) {
-    document.getElementById("demo1").innerHTML = "Input not valid";
-  }
-  else {
-      document.getElementById("wideCam").setAttribute("width", x1);     
-      document.getElementById("demo1").innerHTML = "Width adjusted";
-      
-  }
-}
-
-function wideHeight() {
-  var x2;
-
-  x2 = document.getElementById("wideHeight").value;
-
-  if (isNaN(x2)) {
-    document.getElementById("demo2").innerHTML = "Input not valid";
-  }
-  else {
-      document.getElementById("demo2").innerHTML = "Height adjusted";
-      document.getElementById("wideCam").setAttribute("height", x2);
-  }
-  
-}
-
-function regularWidth() {
-  var x3;
-
-  x3 = document.getElementById("regularWidth").value;
-
-  if (isNaN(x3)) {
-    document.getElementById("demo3").innerHTML = "Input not valid";
-  } 
-  else {
-      document.getElementById("demo3").innerHTML = "Width adjusted";
-      document.getElementById("regularCam").setAttribute("width", x3);
-  }
-}
-
-function regularHeight() {
-  var x4;
-
-  x4 = document.getElementById("regularHeight").value;
-
-  if (isNaN(x4)) {
-    document.getElementById("demo4").innerHTML = "Input not valid";
-  }
-  else {
-      document.getElementById("demo4").innerHTML = "Height adjusted";
-      document.getElementById("regularCam").setAttribute("height", x4);
-  }
-}
+    function drawLine(x, y, stopX, stopY){
+        ctx.clearRect (0, 0, can.width, can.height);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(stopX, stopY);
+        ctx.closePath();
+        ctx.stroke();
+    }
+	
+	drawline(120,20,200,80)
+});
